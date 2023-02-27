8a9b8839ceecc9804888f073ced5e5633bd8c5dc
KyrianTheGhosthand <110039340+KyrianTheGhosthand@users.noreply.github.com>
2023-02-28 05:49:33 +0700

    Add files via upload

---
+++ demofinal/src/main/java/com/example/demofinal/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.example.demofinal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(DemofinalApplication.class);
+    }
+}
